7335b42c1fb828d9883eea0110d1aefacc47db30
Petr Dish <119785172+ArtVandalism@users.noreply.github.com>
2024-08-20 22:10:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ document.querySelectorAll('code').forEach((element) => {
     element.addEventListener('click', () => {
         const text = element.textContent;
         navigator.clipboard.writeText(text).then(() => {
-            alert('Code copied to clipboard!');
+            showTooltip(element, 'Скопировано');
         }).catch(err => {
             console.error('Failed to copy code: ', err);
         });
     });
 });
+
+function showTooltip(element, message) {
+    // Создаем элемент для всплывающего сообщения
+    let tooltip = document.createElement('div');
+    tooltip.className = 'tooltip';
+   ​⬤
