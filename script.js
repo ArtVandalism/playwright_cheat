8aac6209eec300c5840542561cc1dd37e9a3f1d9
Petr Dish <119785172+ArtVandalism@users.noreply.github.com>
2024-08-21 04:35:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+function copyCode(text) {
+    navigator.clipboard.writeText(text).then(() => {
+        alert('Code copied to clipboard!');
+    }).catch(err => {
+        console.error('Failed to copy text: ', err);
+    });
+}
+
 document.querySelectorAll('.copy-button').forEach(button => {
-    button.addEventListener('click', function() {
-        // Находим текст внутри элемента <code>
+    button.addEventListener('click', function(event) {
+        event.stopPropagation(); // Останавливаем событие, чтобы не вызвать обработчик на <code>
         const codeBlock = this.previousElementSibling;
         const codeText = codeBlock.innerText;
+        copyCode(codeText);
+    });
+});
 
-        // Копируем текст в буфер обмена
-        navigator.clipboard.writeText(codeText).then(() => {
-            // Меняем текст кнопки на "Copied!" временно
-            this.innerHTML = 'Copied!';
-            setTimeout(() => {
-                this.innerHTML = '&#128203;'; // Возвращаем оригинальную иконку
-            }, 1500);
-        }).catch(err => {
-            console.error('Failed to copy text: ', err);
-        });
+document.querySelectorAll('.code-container').forEach(container => {
+    container.addEventListener('click', function() {
+        const codeBlock = this.querySelector('code');
+        const codeText = codeBlock.innerText;
+        copyCode(codeText);
     });
 });
