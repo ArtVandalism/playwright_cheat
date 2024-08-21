546468d1185f30f2e2e7b9483f34330ae3dd1a27
Petr Dish <119785172+ArtVandalism@users.noreply.github.com>
2024-08-21 04:37:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function copyCode(text) {
+function copyCode(text, tooltip) {
     navigator.clipboard.writeText(text).then(() => {
-        alert('Code copied to clipboard!');
+        tooltip.classList.add('tooltip-visible');
+        setTimeout(() => {
+            tooltip.classList.remove('tooltip-visible');
+        }, 1500);
     }).catch(err => {
         console.error('Failed to copy text: ', err);
     });
@@ -9,9 +12,10 @@ function copyCode(text) {
 document.querySelectorAll('.copy-button').forEach(button => {
     button.addEventListener('click', function(event) {
         event.stopPropagation(); // Останавливаем событие, чтобы не вызвать обработчик на <code>
-        const codeBlock = this.previousElementSibling;
+        const codeBlock = this.previousElementSibling.querySelector('code');
         const codeText = codeBlock.innerText;
-        copyCode(codeText);
+        const tooltip = this.nextElementSibling;
+        copyCode(codeText, tooltip);
     });
 });
 
@@ -19,6 +23,7 @@ document.querySelectorAll('.code-container').forEach(container => {
     container.addEventListener('click', function() {
         const codeBlock = this.querySelector('code');
         const codeText = codeBlock.innerText;
-        copyCode(codeText);
+        const tooltip = this.querySelector('.tooltip');
+        copyCode(codeText, tooltip);
     });
 });
